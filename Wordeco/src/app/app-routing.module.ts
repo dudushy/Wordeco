714db23ac73cfa2150875219c07220be64802bcc
dudushy <eduardotalarico1337@gmail.com>
2022-05-17 23:25:47 -0300

    Create game skeleton
    Created homepage: word input, buttons grid, keyboard

--- Wordeco/src/app/app-routing.module.ts
+++ Wordeco/src/app/app-routing.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  // },
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./homepage/homepage.module').then( m => m.HomepagePageModule)
+  },
+  {
+    path: 'homepage',
+    loadChildren: () => import('./homepage/homepage.module').then( m => m.HomepagePageModule)
   }
 ];
 @NgModule({
