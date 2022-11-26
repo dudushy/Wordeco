eb78027996dacc1f0b0dbeb96446f5db3efd0188
dudushy <eduardotalarico1337@gmail.com>
2022-11-26 13:59:33 -0300

    Import Platform and Alert + eraseData function

--- src/app/pages/config/config.page.ts
+++ src/app/pages/config/config.page.ts
@@ -5,6 +5,8 @@ import { DbService } from '../../services/db/db.service';
 
 import { AppComponent } from '../../app.component';
 
+import { Platform, AlertController } from '@ionic/angular';
+
 @Component({
   selector: 'app-config',
   templateUrl: './config.page.html',
@@ -16,7 +18,9 @@ export class ConfigPage implements OnInit {
   constructor(
     public db: DbService,
     private cdr: ChangeDetectorRef,
-    public app: AppComponent
+    public app: AppComponent,
+    public platform: Platform,
+    public alertController: AlertController
   ) {
     console.log(`[${this.TITLE}#constructor]`);
   }
@@ -27,6 +31,10 @@ export class ConfigPage implements OnInit {
 
   ionViewDidEnter(): void {
     console.log(`[${this.TITLE}#ionViewDidEnter]`);
+
+    this.platform.ready().then((readySource) => {
+      console.log(`[${this.TITLE}#ionViewDidEnter] readySource`, readySource);
+    });
   }
 
   updateView(from: string): void {
@@ -43,4 +51,56 @@ export class ConfigPage implements OnInit {
   }
 
   defaultSort(): number { return 0; }
+
+  async showAlert(topic: string, msg: string): Promise<any> {
+    console.log(`[${this.TITLE}#showAlert] topic`, topic);
+    console.log(`[${this.TITLE}#showAlert] msg`, msg);
+
+    const alert = await this.alertController.create({
+      cssClass: 'alerts',
+      header: topic,
+      message: msg,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log(`[${this.TITLE}#showAlert] role`, role);
+    return role;
+  }
+
+  async eraseData(topic: string, msg: string): Promise<any> {
+    const alert = await this.alertController.create({
+      cssClass: 'alerts',
+      header: topic,
+      message: msg,
+      buttons: [
+        {
+          cssClass: 'alerts-cancel',
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log(`[${this.TITLE}#eraseData] CANCEL`);
+          }
+        },
+        {
+          cssClass: 'alerts-accept',
+          text: 'Accept',
+          handler: () => {
+            console.log(`[${this.TITLE}#eraseData] ACCEPT`);
+
+            this.db.erase();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log(`[${this.TITLE}#eraseData] role`, role);
+
+    this.updateView(this.TITLE);
+  }
 }
